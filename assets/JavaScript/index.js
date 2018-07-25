d5dd73661ca4a445197cac6d3354856df9107797
Nick <nlweber3@gmail.com>
2018-07-25 13:44:08 -0700

    initial commit

---
+++ assets/JavaScript/index.js
@@ -0,0 +1,17 @@
+//show word bubble on click then hide
+$(document).ready(function () {
+    $('#showHiddenBlock').click(function () {
+        $('#bubble').fadeIn('slow', function () {
+            $('#bubble').delay(5000).fadeOut();
+        });
+    });
+    //time delay on home text
+    $('#delay').hide();
+    setTimeout(function () {
+        $("#delay").show();
+    }, 800);
+    //hover on cards animates them
+    $('.card').hover(
+        function () { $(this).toggleClass('animated tada') }
+    )
+});
